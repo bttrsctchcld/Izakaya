c1f15b92834ca8e4b133b2d2f57a09aff59d04e8
Justin Charity <66874949+bttrsctchcld@users.noreply.github.com>
2020-10-31 18:12:43 -0400

    Update hourly.py

--- hourly.py
+++ hourly.py
@@ -12,3 +12,15 @@ def hourly(self):
             return print_hour
         return hours
     return decorate
+
+@hourly
+def describe_restaurant(self):
+    print(f"{self.name} serves {self.cuisine_type}. The restaurant opens at {print_uptime} and closes at {print_downtime}.")
+    now = datetime.now()
+    current_hour = int(now.strftime("%H"))
+    if (self.uptime < self.downtime and self.uptime <= current_hour < self.downtime) or (self.uptime > self.downtime and (current_hour >= self.uptime or current_hour < self.downtime)):
+        print("The restaurant is currently open.")
+        return True
+    else:
+        print("The restaurant is currently closed.")
+        return False
