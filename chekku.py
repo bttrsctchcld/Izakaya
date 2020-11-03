5a4d7697243bd80cfdafe3a8fc73d51e79408ac0
Justin Charity <66874949+bttrsctchcld@users.noreply.github.com>
2020-11-03 11:50:30 -0500

    Update chekku.py

--- chekku.py
+++ chekku.py
@@ -5,3 +5,17 @@
 # overload __len__ to count the number of orders in a given ticket
 # overload __repr__ to name the restaurant and list the total due as well as total paid
 # however, the receipt should reproduce the ticket to show discrete line item x quantity
+
+from izakaya import Restaurant
+
+class Ticket(Restaurant):
+    def __init__(self,check=None):
+        super().__init__(self)
+        self.line_item = {"order" : None, "price" : 0.00}
+        if check is None:
+            self.check = []
+        else:
+            self.check = list(check)
+        self.total = total
+        self.total = sum([self.line_item["price"] for self.line_item in self.check])
+ 
