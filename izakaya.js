d7cbaee98ed88c0bd30c07a6d8204d4124492b4f
Justin Charity <66874949+bttrsctchcld@users.noreply.github.com>
2020-11-30 09:18:49 -0500

    Update izakaya.js

--- izakaya.js
+++ izakaya.js
@@ -19,5 +19,5 @@ class Restaurant {
  * in js, unlike python, classes can be elaborated beyond the initializing scope;
  * you can spread the functions out and thus avoid the need to create two subclasses
  *
- * be mindful of the differences between: js objects vs python dictionaries (valid keys, index notation),
+ * be mindful of the differences between: js maps vs python dictionaries (valid keys, index notation),
  * js arrays vs python lists, js classes and prototypes vs python classes (null, new) */
